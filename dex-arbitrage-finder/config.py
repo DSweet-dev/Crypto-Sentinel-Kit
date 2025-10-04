ed8b294968758152dd9460f4376befbc3d7f1739
Dmitriy Solodkyi <53041608+DSweet-dev@users.noreply.github.com>
2025-10-04 16:53:57 +0300

    Create config.py

---
+++ dex-arbitrage-finder/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+NODE_HTTP_URL = os.getenv("NODE_HTTP_URL", "https://mainnet.infura.io/v3/your-infura-key")
+
+# Адреси токенів
+WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+USDC_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+
+# Адреси роутерів DEX
+UNISWAP_V2_ROUTER = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+SUSHISWAP_ROUTER = "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F"
+
+# Простий ABI для функції getAmountsOut
+ROUTER_ABI = '[{"inputs":[{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"address[]","name":"path","type":"address[]"}],"name":"getAmountsOut","outputs":[{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"stateMutability":"view","type":"function"}]'
